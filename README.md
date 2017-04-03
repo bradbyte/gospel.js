bec43abfcaa4000be41953c5928dc49b697157cd
Brad Bumbalough <bradley.bumbalough@gmail.com>
2017-04-03 06:37:05 -0400

    added readme, and thoughts.js

---
+++ README.md
@@ -0,0 +1,7 @@
+# Gospel.js
+
+As a developer, I often picture things in life as code. The abstraction and representation of truths in methods and variables helps to understand relationships, cause / affect, and outcomes.
+
+The most important truth I've discovered is that of the Gospel of Jesus Christ -- namely, that I cannot do enough good things to earn any kind of salvation, and instead, Christ took my sin upon himself personally and in turn gives me his own righteousness. 
+
+I've wanted to turn these truths into a coded example for a long time, but this will be a WIP and not be just limited to the gospel.
---
+++ thoughts.js
@@ -0,0 +1,49 @@
+const TRUE = 'true',
+      HONORABLE = 'honorable',
+      JUST = 'just',
+      PURE = 'pure',
+      LOVELY = 'lovely',
+      COMMENDABLE = 'commendable',
+      EXCELLENT = 'excellent',
+      PRAISEWORTHY = 'praiseworthy';
+
+class Thoughts {
+  constructor(thoughts) {
+    this.thoughts = thoughts;
+  }
+
+  think() {
+    this.thoughts.filter(thought => thought.shouldThink())
+  }
+
+}
+
+class Thing {
+  constructor(thing, attributes) {
+    this.thing = thing;
+    this.attributes = attributes;
+  }
+
+  shouldThink() {
+    /**
+     * Philippianss 4:8-9, ESV
+     * Finally, brothers, whatever is true, whatever is honorable, whatever is just, 
+     * whatever is pure, whatever is lovely, whatever is commendable, if there is any 
+     * excellence, if there is anything worthy of praise, think about these things. 
+     * What you have learned and received and heard and seen in me—practice these things, 
+     * and the God of peace will be with you.
+     */
+    return this.attributes.filter(attr => attr == TRUE || 
+                                          attr == HONORABLE ||
+                                          attr == JUST ||
+                                          attr == PURE ||
+                                          attr == LOVELY ||
+                                          attr == COMMENDABLE ||
+                                          attr == EXCELLENT ||
+                                          attr == PRAISEWORTHY
+                                          ).length > 0;
+  }
+}
+
+exports.Thoughts = Thoughts;
+exports.Thought = Thought;
